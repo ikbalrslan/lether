cf09a958f2ec07e2b803b047005ac7f37983f381
Ahmet Samil Arslan <arslanahmetsamil@gmail.com>
2024-05-26 18:57:51 +0300

    try new branch

--- src/app/components/AboutUs.tsx
+++ src/app/components/AboutUs.tsx
@@ -17,8 +17,9 @@ const AboutUs = () => {
         <div className="">
           <div className="m-10 text-justify">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum id
-            ex sit laboriosam dolores consectetur quia inventore minima dolorem
-            quasi corrupti repellendu
+            ex sit laboriosam dolores consectetur qu
+            corrupti rerum earum quaerat fugiat nam dolorem! Eligendi a quasi
+            deserunt qui nulla pariatur, sunt sed minus.
           </div>
           <div className="mx-8">
             <Image
